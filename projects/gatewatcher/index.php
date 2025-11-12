9ce2485459d96eb7751e14b69693e77f8ac8e349
Craig Bavender <craigbavender@Mac.indy.rr.com>
2025-11-11 23:22:36 -0500

    Basic idea data submission template

--- projects/gatewatcher/index.php
+++ projects/gatewatcher/index.php
@@ -1,1 +1,31 @@
-<?php echo "Hello World"; 
+<?php 
+
+?>
+
+<html>
+
+<body>
+
+<h1>Submit Results</h1>
+<form name="submit_results">
+	<div class="deck-and-player">
+		<input type="text" name="player_one" placeholder="Player 1st">
+		<input type="text" name="commander_one" placeholder="Commander">
+	</div>
+	<div class="deck-and-player">
+		<input type="text" name="player_two" placeholder="Player 2nd">
+		<input type="text" name="commander_two" placeholder="Commander">
+	</div>
+	<div class="deck-and-player">
+		<input type="text" name="player_three" placeholder="Player 3rd">
+		<input type="text" name="commander_three" placeholder="Commander">
+	</div>
+	<div class="deck-and-player">
+		<input type="text" name="player_four" placeholder="Player 4th">
+		<input type="text" name="commander_four" placeholder="Commander">
+	</div>
+</form>
+
+</body>
+
+</html>
